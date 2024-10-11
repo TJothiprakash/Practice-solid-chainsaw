71d274d7f335391e6bad96744e7985a3bba4fc91
jothiprakash <jothiprakash888@gmail.com>
2024-10-11 09:34:00 +0530

    oct 11 morning session

--- src/dsa_assignments_kunal/sept_26/PracticePattern11_15.java
+++ src/dsa_assignments_kunal/sept_26/PracticePattern11_15.java
@@ -1,7 +1,46 @@
 package dsa_assignments_kunal.sept_26;
 
+
+/**/
 public class PracticePattern11_15 {
     public static void main(String[] args) {
+  new PracticePattern11_15().printReverseTriangle(5);
+    }
+
+    //pattern 11
+    public void printReverseTriangle(int n){
+        for (int i = n; i >=0; i--) {
+            for (int k = i; k <=n ; k++) {
+                System.out.print(" ");
+            }
+            for (int j = i; j >=0 ; j--) {
+                System.out.print(" *");
+            }
+            System.out.println();
+        }
+    }
+
+    //pattern 12
+    public void printtopandbottomTriangle(int n){
+        for (int i = n; i >0; i--) {
+            for (int k = i; k <=n ; k++) {
+                System.out.print(" ");
+            }
+            for (int j = i; j >0 ; j--) {
+                System.out.print(" *");
+            }
+            System.out.println();
+        }
+        for (int i = 0; i <n; i--) {
+            for (int k = i; k <n ; k++) {
+                System.out.print(" ");
+            }
+            for (int j = i; j <0 ; j--) {
+                System.out.print(" *");
+            }
+            System.out.println();
+        }
 
     }
+
 }
